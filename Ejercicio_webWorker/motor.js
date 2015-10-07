5144d75fe6c5ab22699f3d73603daa97562ddad9
Alejandrogf <alejandrogarciaf@gmail.com>
2015-10-07 12:10:59 +0200

    Ejercicio webworkers reloj

--- Ejercicio_webWorker/motor.js
+++ Ejercicio_webWorker/motor.js
@@ -1,1 +1,30 @@
-﻿
+﻿var worker = undefined;
+
+function crearWorker() {
+    worker = new Worker("wwReloj.js");
+    worker.onmessage=function(evento) {
+        var capa = document.getElementById("reloj");
+        if (evento.data.accion === 1) {
+            capa.innerHTML = evento.data.contenido;
+        }
+        else if (evento.data.accion === 2) {
+            capa.style.color = evento.data.contenido;
+
+        }
+    }
+}
+
+function cambiarColor() {
+    var data = { accion: 2 };
+    worker.postMessage(data);
+}
+
+function cambiarZona() {
+    var data = { accion: 1 };
+    worker.postMessage(data);
+}
+
+crearWorker();
+
+document.getElementById("btnHora").addEventListener("click", cambiarZona);
+document.getElementById("btnColor").addEventListener("click", cambiarColor);
--- Ejercicio_webWorker/wwReloj.js
+++ Ejercicio_webWorker/wwReloj.js
@@ -1,1 +1,43 @@
-﻿
+﻿var insular = false;
+
+function hora() {
+    var d = new Date();
+
+    var h = (!insular ? d.getHours() : (d.getHours()-1)) + ":" + d.getMinutes() + ":" + d.getSeconds();
+
+    postMessage({ accion: 1, contenido: h });
+    setTimeout("hora()", 500);
+}
+
+function color() {
+    var r = parseInt(Math.random() * 255);
+    var g = parseInt(Math.random() * 255);
+    var b = parseInt(Math.random() * 255);
+
+    var col = "rgb(" + r + "," + g + "," + b + ")";
+
+    postMessage({ accion: 2, contenido: col  });
+
+
+}
+
+
+function zona() {
+    //insular = !insular;
+    if (insular === true) {
+        insular = false;
+    } else {
+        insular = true;
+    }
+}
+
+hora();
+
+self.onmessage=function(evento) {
+    if (evento.data.accion === 1) {
+        zona();
+    }
+    else if (evento.data.accion === 2) {
+        color();
+    }
+}
